f0b64aa1ef23e345eb4a3b41c5f04bc4d7fb8ef5
Chirag V <chirag05k@gmail.com>
2025-08-25 18:25:44 +0530

    initial commit

---
+++ BubbleAnimation.swift
@@ -0,0 +1,75 @@
+//
+//  BubbleAnimation.swift
+//  HIKE-SwiftUI
+//
+//  Created by Chirag V on 25/08/25.
+//
+
+import SwiftUI
+
+struct BubbleAnimation: View {
+    
+    // MARK: - Properties
+    @State private var randomCircle = Int.random(in: 20...30)
+    @State private var isAnimating = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.indigo.ignoresSafeArea()
+                
+                ForEach(0...randomCircle, id: \.self) { _ in
+                    Circle()
+                        .foregroundStyle(.white)
+                        .opacity(0.25)
+                        .frame(width: randomSize())
+                        .position(
+                            x: randomCoordinate(max: geometry.size.width),
+                            y: randomCoordinate(max: geometry.size.height)
+                        )
+                        .scaleEffect(isAnimating ? randomScale() : 1)
+                        .onAppear {
+                            withAnimation(
+                                .interpolatingSpring(stiffness: 0.25, damping: 0.25)
+                                .repeatForever()
+                                .speed(randomSpeed())
+                                .delay(randomDelay())
+                            ) {
+                                isAnimating = true
+                            }
+                        }
+                }
+            }
+            .drawingGroup()
+        }
+        .ignoresSafeArea()
+    }
+    
+    // MARK: - Functions
+    
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 40...120))
+    }
+    
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.5...2.0))
+    }
+    
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.05...1.0)
+    }
+    
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+}
+
+struct BubbleAnimation_Preview: PreviewProvider {
+    static var previews: some View {
+        BubbleAnimation()
+    }
+}
